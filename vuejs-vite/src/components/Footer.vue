0e26ed71dbbb30767ab690802368e530afdb5d57
Damar Buana Murti <damarbuanamurti@gmail.com>
2025-02-20 14:53:16 +0700

    feat: implement Header, Home, and Footer

---
+++ vuejs-vite/src/components/Footer.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="block">
+            <p>
+              Copyright &copy;
+              <a href="http://www.Themefisher.com">Themefisher</a>| All right
+              reserved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style></style>
