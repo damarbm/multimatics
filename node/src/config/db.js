40f5f75478b942f25e6957d6d0f03d507dc896f0
Damar Buana Murti <damarbuanamurti@gmail.com>
2025-02-24 15:16:35 +0700

    feat: final

--- node/src/config/db.js
+++ node/src/config/db.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASS,
-  { host: process.env.DB_HOST, dialect: "mysql" }
+  process.env.DB_PASSWORD,
+  { host: process.env.DB_HOST, dialect: "mariadb" }
 );
 
 const connectDB = async () => {
